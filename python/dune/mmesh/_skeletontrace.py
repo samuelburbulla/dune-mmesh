27c82bdba297034bc0d7a27224b33a8a140628c7
dedner <a.s.dedner@warwick.ac.uk>
2020-06-29 09:45:42 +0100

    export both trace functions in single module - needs update in dune-common

--- python/dune/mmesh/_skeletontrace.py
+++ python/dune/mmesh/_skeletontrace.py
@@ -39,39 +39,43 @@ def trace(bulkFunction):
     try:
         return bulkFunction.trace
     except:
-        igrid = bulkFunction.space.grid.hierarchicalGrid.interfaceGrid
+        pass
 
-        includes = ["dune/mmesh/misc/pyskeletontrace.hh"]
-        includes += bulkFunction._includes
-        generator = SimpleGenerator("TraceGF", "Dune::Fem")
+    igrid = bulkFunction.space.grid.hierarchicalGrid.interfaceGrid
 
-        traces = {}
-        for side in [ "in", "out" ]:
-            sideStr = "Dune::Fem::IntersectionSide::" + side
-            typeName = "Dune::Fem::TraceGF< " + igrid._typeName + ", " + bulkFunction._typeName + ", " + sideStr + " >"
-            moduleName = "skeleton_" + hashlib.md5(typeName.encode('utf8')).hexdigest()
-            cls = generator.load(includes, typeName, moduleName)
-            traces[side] = cls.TraceGF(igrid, bulkFunction)
+    traces = {}
+    includes = ["dune/mmesh/misc/pyskeletontrace.hh"]
+    includes += bulkFunction._includes
+    generator = SimpleGenerator(["TraceGF","TraceGF"], "Dune::Fem", pythonname=["TraceGFP","TraceGFM"])
+    typeName = []
+    for side in [ "in", "out" ]:
+        sideStr = "Dune::Fem::IntersectionSide::" + side
+        typeName += ["Dune::Fem::TraceGF< " + igrid._typeName + ", " + bulkFunction._typeName + ", " + sideStr + " >"]
+    moduleName = "skeleton_"+hashlib.md5(''.join(typeName).encode('utf8')).hexdigest()
+    module = generator.load(includes, typeName, moduleName,
+                            options=[[],[]]) # options can be removed but requires fix in dune-common
+    traces["in"]  = module.TraceGFP(igrid, bulkFunction)
+    traces["out"] = module.TraceGFM(igrid, bulkFunction)
 
-        trace_p = dune.ufl.GridFunction(traces["in"])
-        trace_m = dune.ufl.GridFunction(traces["out"])
-        if bulkFunction.scalar:
-            trace_p = trace_p.toVectorCoefficient()
-            trace_m = trace_m.toVectorCoefficient()
-        trace = trace_p
-        predefined = {}
-        predefined[trace('+')]                     = trace_p
-        predefined[trace('-')]                     = trace_m
-        predefined[ufl.grad(trace)('+')]           = ufl.grad(trace_p)
-        predefined[ufl.grad(trace)('-')]           = ufl.grad(trace_m)
-        predefined[ufl.grad(ufl.grad(trace))('+')] = ufl.grad(ufl.grad(trace_p))
-        predefined[ufl.grad(ufl.grad(trace))('-')] = ufl.grad(ufl.grad(trace_m))
-        predefined[ufl.grad(ufl.grad(ufl.grad(trace)))('+')] = ufl.grad(ufl.grad(ufl.grad(trace_p)))
-        predefined[ufl.grad(ufl.grad(ufl.grad(trace)))('-')] = ufl.grad(ufl.grad(ufl.grad(trace_m)))
-        trace.predefined = predefined
-        if bulkFunction.scalar:
-            trace = trace[0]
+    trace_p = dune.ufl.GridFunction(traces["in"])
+    trace_m = dune.ufl.GridFunction(traces["out"])
+    if bulkFunction.scalar:
+        trace_p = trace_p.toVectorCoefficient()
+        trace_m = trace_m.toVectorCoefficient()
+    trace = trace_p
+    predefined = {}
+    predefined[trace('+')]                     = trace_p
+    predefined[trace('-')]                     = trace_m
+    predefined[ufl.grad(trace)('+')]           = ufl.grad(trace_p)
+    predefined[ufl.grad(trace)('-')]           = ufl.grad(trace_m)
+    predefined[ufl.grad(ufl.grad(trace))('+')] = ufl.grad(ufl.grad(trace_p))
+    predefined[ufl.grad(ufl.grad(trace))('-')] = ufl.grad(ufl.grad(trace_m))
+    # predefined[ufl.grad(ufl.grad(ufl.grad(trace)))('+')] = ufl.grad(ufl.grad(ufl.grad(trace_p)))
+    # predefined[ufl.grad(ufl.grad(ufl.grad(trace)))('-')] = ufl.grad(ufl.grad(ufl.grad(trace_m)))
+    trace.predefined = predefined
+    if bulkFunction.scalar:
+        trace = trace[0]
 
-        bulkFunction.trace = trace
-        return trace
+    bulkFunction.trace = trace
+    return trace
 ################################################################################
