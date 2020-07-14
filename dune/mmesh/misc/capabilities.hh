e9a4a93c0ef87e641442f8838ab9446e9047c06c
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2020-07-14 11:35:17 +0200

    add capabilities

---
+++ dune/mmesh/misc/capabilities.hh
@@ -0,0 +1,49 @@
+#ifndef DUNE_MMESH_MISC_CAPABILITIES_HH
+#define DUNE_MMESH_MISC_CAPABILITIES_HH
+
+#if HAVE_DUNE_FEM
+
+#include <dune/fem/misc/capabilities.hh>
+
+namespace Dune
+{
+
+  namespace Capabilities
+  {
+
+    template< class HostGrid, int dim >
+    struct hasHierarchicIndexSet< MMesh< HostGrid, dim > >
+    {
+      static const bool v = false;
+    };
+
+  } // namespace Capabilities
+
+
+  namespace Fem
+  {
+
+    namespace Capabilities
+    {
+
+      template< class HostGrid, int dim >
+      struct supportsCallbackAdaptation< MMesh< HostGrid, dim > >
+      {
+        static const bool v = true;
+      };
+
+      template< class HostGrid, int dim >
+      struct isLocallyAdaptive< MMesh< HostGrid, dim > >
+      {
+        static const bool v = true;
+      };
+
+    } // namespace Capabilities
+
+  } // namespace Fem
+
+} // namespace Dune
+
+#endif // #if HAVE_DUNE_FEM
+
+#endif // #if DUNE_MMESH_MISC_CAPABILITIES_HH
