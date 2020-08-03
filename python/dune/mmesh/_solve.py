502ecadf9196fbedf68f1af5d286f971ad300a3d
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2020-08-03 19:29:37 +0200

    Add monolithic newton.

--- python/dune/mmesh/_solve.py
+++ python/dune/mmesh/_solve.py
@@ -7,6 +7,56 @@ from scipy.sparse import linalg, csc_matrix
 import dune.ufl
 from dune.fem.operator import linear as linearOperator
 
+def monolithicNewton(schemes, targets, iter=10000, f_rtol=1e-8, verbose=False, **kwargs):
+    """solve bulk and interface scheme coupled monolithically using scipy's newton krylov method
+
+    Args:
+        schemes: pair of schemes
+        targets: pair of discrete functions that should be solved for AND that are used in the coupling forms
+        kwargs:  additional arguments passed to the newton_krylov call
+    """
+    assert len(schemes) == 2
+    assert len(targets) == 2
+
+    (scheme, ischeme) = schemes
+    (ph, ih) = targets
+
+    jac = linearOperator(scheme)
+    ijac = linearOperator(ischeme)
+
+    n = len(ph.as_numpy)
+    m = len(ih.as_numpy)
+
+    Ax = ph.copy()
+    iAx = ih.copy()
+
+    def F(x):
+        ph.as_numpy[:] = x[:n]
+        ih.as_numpy[:] = x[n:]
+        scheme(ph, Ax)
+        ischeme(ih, iAx)
+        return np.concatenate((Ax.as_numpy, iAx.as_numpy))
+
+    x = np.concatenate((ph.as_numpy, ih.as_numpy))
+    f = F(x)
+    for i in range(iter):
+        jacp = linalg.spsolve(jac.as_numpy, f[:n])
+        ijacp = linalg.spsolve(ijac.as_numpy, f[n:])
+
+        x[:n] -= jacp
+        x[n:] -= ijacp
+
+        f = F(x)
+        if verbose:
+            print("i:", i, "  res =", np.sqrt(np.dot(f, f)))
+
+        if np.dot(f, f) < f_rtol**2:
+            break
+
+    ph.as_numpy[:] = x[:n]
+    ih.as_numpy[:] = x[n:]
+
+
 def monolithicSolve(schemes, targets, solver=linalg.gmres, preconditioner=linalg.spilu, **kwargs):
     """solve bulk and interface scheme coupled monolithically
 
