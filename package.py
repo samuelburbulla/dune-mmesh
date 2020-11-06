6d61ed680764dc6a26f924d0144e938abcacde1d
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2020-11-06 17:23:57 +0100

    Update package.py.

--- package.py
+++ package.py
@@ -1,13 +1,16 @@
 # Meta data
 name="dune-mmesh"
-version="2.8.201015"
+version="2.8"
 author="Samuel Burbulla"
 author_email="samuel.burbulla@mathematik.uni-stuttgart.de"
 description="MMesh is a grid implementation based on CGAL triangulations."
 url="https://gitlab.dune-project.org/samuel.burbulla/dune-mmesh"
 
+# DUNE dependencies
+dune_dependencies=['dune-grid']
+
 # Package dependencies
-install_requires=['dune-grid']
+install_requires=[]
 
 # Module libaries that have to be compiled (without the _ prefix)
 modules=['mmesh']
@@ -25,6 +28,5 @@ include CMakeLists.txt
 include dune-mmesh.pc.in
 include dune.module
 include LICENSE.md
-include pyproject.toml
 include README.md
 '''
