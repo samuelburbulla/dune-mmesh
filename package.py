51a21d51951c7ad34cd58c6bcd5c8948f63a41c7
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2020-11-24 11:16:02 +0100

    Remove manifest from package.py.

--- package.py
+++ package.py
@@ -5,24 +5,7 @@ author="Samuel Burbulla"
 author_email="samuel.burbulla@mathematik.uni-stuttgart.de"
 description="MMesh is a grid implementation based on CGAL triangulations."
 url="https://gitlab.dune-project.org/samuel.burbulla/dune-mmesh"
+install_requires=[]
 
 # DUNE dependencies
 dune_dependencies=['dune-grid']
-
-# Package dependencies
-install_requires=[]
-
-# Files to include in the source package
-manifest='''\
-graft cmake
-graft doc
-graft dune
-graft examples
-graft python
-graft scripts
-include CMakeLists.txt
-include dune-mmesh.pc.in
-include dune.module
-include LICENSE.md
-include README.md
-'''
