77d43eecb026265f39a1c9469a8e818a2c03ae06
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2021-04-13 15:52:34 +0200

    Align version numbers.

--- setup.py
+++ setup.py
@@ -3,4 +3,24 @@ try:
 except ImportError:
     from packagemetadata import metaData
 from skbuild import setup
-setup(**metaData()[1])
+
+mmeshVersion = '1.2.0.dev20210413'
+duneVersion  = '2.8.0.dev20210318'
+
+metadata = metaData(duneVersion)[1]
+metadata['version'] = mmeshVersion
+
+# auto-generate pyproject.toml with duneVersion when building sdist
+from skbuild.command.sdist import sdist
+class mysdist(sdist):
+    def run(self):
+        requires = ['setuptools', 'wheel', 'scikit-build', 'cmake', 'ninja', 'requests']
+        requires += metadata['install_requires']
+        with open('pyproject.toml', 'w') as f:
+            f.write("[build-system]\n")
+            f.write("requires = ['"+"', '".join(requires)+"']\n")
+            f.write("build-backend = 'setuptools.build_meta'\n")
+        sdist.run(self)
+metadata['cmdclass'] = {'sdist': mysdist}
+
+setup(**metadata)
