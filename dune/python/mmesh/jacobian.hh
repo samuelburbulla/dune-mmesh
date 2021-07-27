a76c7c68476ef1a69e02262dba3ea4893df790fb
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2021-07-27 14:40:59 +0200

    [WIP] Continue but stuck.

--- dune/python/mmesh/jacobian.hh
+++ dune/python/mmesh/jacobian.hh
@@ -31,31 +31,22 @@ namespace Dune
        *         and interface edges ien.
        */
       template <class DomainSpace, class RangeSpace, class Partition = Dune::Partitions::InteriorBorder>
-      struct InterfaceNeighborStencil : public Fem::Stencil<DomainSpace,RangeSpace>
+      struct InterfaceNeighborStencil : public Fem::Stencil<DomainSpace, RangeSpace>
       {
-        typedef Fem::Stencil<DomainSpace,RangeSpace> BaseType;
+        typedef Fem::Stencil<DomainSpace, RangeSpace> BaseType;
       public:
-        typedef Partition                                 PartitionType;
-        typedef typename BaseType::DomainEntityType       DomainEntityType;
-        typedef typename BaseType::RangeEntityType        RangeEntityType;
-        typedef typename BaseType::DomainGlobalKeyType    DomainGlobalKeyType;
-        typedef typename BaseType::RangeGlobalKeyType     RangeGlobalKeyType;
-        typedef typename BaseType::LocalStencilType       LocalStencilType;
-        typedef typename BaseType::GlobalStencilType      GlobalStencilType;
-
         InterfaceNeighborStencil(const DomainSpace &dSpace, const RangeSpace &rSpace)
           : BaseType( dSpace, rSpace )
         {
-          const auto& gridPart = rSpace.gridPart();
-          const auto& grid = gridPart.grid();
-          for (const auto & entity: elements(gridPart, PartitionType{}) )
+          const auto& gridPart = dSpace.gridPart();
+          for( const auto& entity : elements(gridPart, Partition{}) )
           {
-            for (const auto & intersection: intersections(gridPart, entity) )
+            for( const auto& intersection : intersections(gridPart, entity) )
             {
-              if( intersection.neighbor() && grid.isInterface(intersection) )
+              if( intersection.neighbor() && gridPart.grid().isInterface(intersection) )
               {
-                auto ientity = grid.asInterfaceEntity(intersection);
-                BaseType::fill(ientity, entity);
+                auto ientity = gridPart.grid().asInterfaceEntity(intersection);
+                BaseType::fill(entity, ientity);
               }
             }
           }
@@ -64,8 +55,12 @@ namespace Dune
 
       template< class Sch, class ISch, class Sol, class ISol >
       class Jacobian
+       : public Fem::Impl::GalerkinOperator< typename Sch::ModelType >
       {
       public:
+        using Base = Fem::Impl::GalerkinOperator< typename Sch::ModelType >;
+        using typename Base::DomainValueVectorType;
+
         using Scheme = Sch;
         using IScheme = ISch;
         using Solution = Sol;
@@ -77,9 +72,9 @@ namespace Dune
         using DType = typename IScheme::JacobianOperatorType;
 
         Jacobian( const Scheme &scheme, const IScheme &ischeme, const Solution &uh, const ISolution &th )
-         : scheme_(scheme), ischeme_(ischeme),
+         : Base(scheme.gridPart(), scheme.model()),
+           scheme_(scheme), ischeme_(ischeme),
            A_( "A", uh.space(), uh.space() ),
-           // B_( "B", th.space(), uh.space() ),
            B_( "B", th.space(), uh.space() ),
            C_( "C", uh.space(), th.space() ),
            D_( "D", th.space(), th.space() )
@@ -90,8 +85,8 @@ namespace Dune
           scheme_.jacobian(uh, A_);
           ischeme_.jacobian(th, D_);
 
-          assemble(scheme_, uh, th, B_);
-          // assemble(ischeme_, th, uh, C_);
+          // assemble(scheme_, th, uh, B_);
+          assemble(ischeme_, uh, th, C_);
         }
 
         void solve( const Solution &f, const ISolution &g, const Solution& ux, const ISolution& tx ) const
@@ -100,24 +95,64 @@ namespace Dune
         }
 
       private:
-        typedef typename Scheme::ModelType::DomainValueType DomainValueType;
-        typedef typename Scheme::ModelType::RangeValueType RangeValueType;
-        typedef std::make_index_sequence< std::tuple_size< DomainValueType >::value > DomainValueIndices;
-        typedef std::make_index_sequence< std::tuple_size< RangeValueType >::value > RangeValueIndices;
 
-        template< std::size_t... i >
-        static auto makeDomainValueVector ( std::size_t maxNumLocalDofs, std::index_sequence< i... > )
+        template< class Scheme, class GridPart, class Intersection, class U, class T, class J, class K >
+        void addLinearizedSkeletonIntegral ( const Scheme &scheme, const GridPart &gridPart, const Intersection &intersection, const U &uIn, const T &uOut,
+                                             DomainValueVectorType &phiIn, DomainValueVectorType &phiOut, J &jInIn, K &jOutIn ) const
         {
-          return std::make_tuple( std::vector< std::tuple_element_t< i, DomainValueType > >( maxNumLocalDofs )... );
+          const auto &domainBasisIn = jInIn.domainBasisFunctionSet();
+          const auto &domainBasisOut = jOutIn.rangeBasisFunctionSet();
+
+          const auto &rangeBasisIn = jInIn.rangeBasisFunctionSet();
+
+          const int order = std::max( std::max( this->maxOrder(uIn), this->maxOrder(uOut) ), this->maxOrder( domainBasisIn, domainBasisOut, rangeBasisIn ) );
+
+          const auto geometry = intersection.geometry();
+          typedef typename Base::template QuadratureSelector< typename J::RangeSpaceType >::SurfaceQuadratureType SurfaceQuadratureType;
+          const SurfaceQuadratureType quadrature( gridPart, intersection, Base::surfaceQuadratureOrder(order), SurfaceQuadratureType::INSIDE );
+
+          typedef Fem::CachingQuadrature< typename Scheme::GridPartType, 0, Fem::Capabilities::DefaultQuadrature< typename J::RangeSpaceType >::template DefaultQuadratureTraits> InteriorQuadratureType;
+          const InteriorQuadratureType iquadrature( uOut.entity(), Base::interiorQuadratureOrder(order) );
+
+          for( std::size_t qp = 0, nop = quadrature.nop(); qp != nop; ++qp )
+          {
+            const auto weight = quadrature.weight( qp ) * geometry.integrationElement( quadrature.localPoint( qp ) );
+
+            const auto qpIn = quadrature[ qp ];
+            const auto qpOut = iquadrature[ qp ];
+
+            this->values( domainBasisIn, qpIn, phiIn );
+            this->values( domainBasisOut, qpOut, phiOut );
+
+            typename Base::RangeValueType rangeValue;
+            this->value( uOut, qpOut, rangeValue );
+
+            auto integrand = linearizedSkeleton( scheme.model(), qpIn, this->domainValue( uIn, qpIn ), qpOut, rangeValue );
+            for( std::size_t col = 0, cols = domainBasisIn.size(); col < cols; ++col )
+            {
+              Fem::LocalMatrixColumn< J > jInInCol( jInIn, col );
+              auto intPhi = integrand.first( this->value( phiIn, col ) );
+
+              Hybrid::forEach( typename Base::RangeValueIndices(), [ &qpIn, &jInInCol, &intPhi, weight ] ( auto i ) {
+                  std::get< i >( intPhi.first ) *= weight;
+                  jInInCol.axpy( qpIn, std::get< i >( intPhi.first ) );
+                } );
+            }
+            for( std::size_t col = 0, cols = domainBasisOut.size(); col < cols; ++col )
+            {
+              Fem::LocalMatrixColumn< J > jOutInCol( jOutIn, col );
+              auto intPhi = integrand.second( this->value( phiOut, col ) );
+
+              Hybrid::forEach( typename Base::RangeValueIndices(), [ &qpIn, &jOutInCol, &intPhi, weight ] ( auto i ) {
+                  std::get< i >( intPhi.first ) *= weight;
+                  jOutInCol.axpy( qpIn, std::get< i >( intPhi.first ) );
+                } );
+            }
+          }
         }
 
-        static auto makeDomainValueVector ( std::size_t maxNumLocalDofs )
-        {
-          return makeDomainValueVector( maxNumLocalDofs, DomainValueIndices() );
-        }
-
-        template< class Scheme, class GridFunction, class InterfaceGridFunction, class JacobianOperator >
-        void assemble (const Scheme &scheme, const GridFunction &u, const InterfaceGridFunction &t, JacobianOperator &jOp) const
+        template< class Scheme, class DomainGridFunction, class RangeGridFunction, class JacobianOperator >
+        void assemble (const Scheme &scheme, const DomainGridFunction &u, const RangeGridFunction &t, JacobianOperator &jOp) const
         {
           typedef typename JacobianOperator::DomainSpaceType  DomainSpaceType;
           typedef typename JacobianOperator::RangeSpaceType   RangeSpaceType;
@@ -133,35 +168,32 @@ namespace Dune
           jOp.clear();
 
           const std::size_t maxNumLocalDofs = jOp.domainSpace().blockMapper().maxNumDofs() * jOp.domainSpace().localBlockSize;
-          auto phiIn = makeDomainValueVector( maxNumLocalDofs );
-          auto phiOut = makeDomainValueVector( maxNumLocalDofs );
+          auto phiIn = Base::makeDomainValueVector( maxNumLocalDofs );
+          auto phiOut = Base::makeDomainValueVector( maxNumLocalDofs );
 
           TemporaryLocalMatrixType jOpInIn( jOp.domainSpace(), jOp.rangeSpace() ), jOpOutIn( jOp.domainSpace(), jOp.rangeSpace() );
-          Dune::Fem::ConstLocalFunction< GridFunction > uIn( u );
+          Dune::Fem::ConstLocalFunction< DomainGridFunction > uIn( u );
 
-          const auto &grid = scheme.gridPart().grid();
-          const auto &indexSet = scheme.gridPart().indexSet();
-          for( const auto &inside : elements( scheme.gridPart(), Partitions::interiorBorder ) )
+          const auto &gridPart = u.gridPart();
+          const auto &grid = gridPart.grid();
+          for( const auto &inside : elements( gridPart, Partitions::interiorBorder ) )
           {
             uIn.bind( inside );
 
-            // jOpInIn.init( inside, inside );
-            // jOpInIn.clear();
-
-            for( const auto &intersection : intersections( scheme.gridPart(), inside ) )
+            for( const auto &intersection : intersections( gridPart, inside ) )
             {
               if( intersection.neighbor() && grid.isInterface( intersection ) )
               {
                 const auto &outside = grid.asInterfaceEntity( intersection );
 
-                jOpOutIn.init( outside, inside );
+                jOpOutIn.init( inside, outside );
                 jOpOutIn.clear();
 
-                Dune::Fem::ConstLocalFunction< InterfaceGridFunction > uOut( t );
-                uOut.bind( outside );
+                Dune::Fem::ConstLocalFunction< RangeGridFunction > tOut( t );
+                tOut.bind( outside );
 
-                scheme.addLinearizedSkeletonIntegral( intersection, uIn, uOut, phiIn, phiOut, jOpInIn, jOpOutIn );
-                jOp.addLocalMatrix( outside, inside, jOpOutIn );
+                addLinearizedSkeletonIntegral( scheme, gridPart, intersection, uIn, tOut, phiIn, phiOut, jOpInIn, jOpOutIn );
+                jOp.addLocalMatrix( inside, outside, jOpOutIn );
               }
             }
           }
