abdfec4be85b0522cc376ac7fa37da3a3a01491b
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2021-09-09 16:00:48 +0200

    Improve test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,47 +1,20 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Coupling to the interface
-#
-# This is an example of how to solve coupled problems on the bulk and interface grid.
-
-# ## Grid creation
-#
-# We use the [horizontal](grids/horizontal.rst) grid file that contains an interface $\Gamma = [0.25, 0.75] \times {0.5}$ embedded in a domain $\Omega = [0,1]^2$.
-
-# In[1]:
-
-
+# Grid creation
 from dune.grid import reader
 from dune.mmesh import mmesh
 dim = 2
-file = "grids/horizontal.msh"
+file = "horizontal.msh"
 gridView  = mmesh((reader.gmsh, file), dim)
 igridView = gridView.hierarchicalGrid.interfaceGrid
 
 
-# ## Solve a problem on the bulk grid
 
-# Let us solve the Poisson equation
-#
-# \begin{align}
-# -\Delta u = f & \qquad \text{in}\ \Omega
-# \end{align}
-#
-# on the bulk grid and on the interface grid. We use the manufactured solution
-# \begin{align}
-# u &= \sin(4 \pi x y), \\
-# f &= -\operatorname{div}( \nabla u ).
-# \end{align}
-
-# In[2]:
-
-
+# Bulk problem
 from ufl import *
 from dune.ufl import DirichletBC
 from dune.fem.space import lagrange
 from dune.fem.scheme import galerkin
 from dune.fem.function import integrate
+import time
 
 space = lagrange(gridView, order=3)
 u = TrialFunction(space)
@@ -54,23 +27,20 @@ f = -div(grad(exact))
 a = inner(grad(u), grad(v)) * dx
 b = f * v * dx
 
+start_time = time.time()
 scheme = galerkin([a == b, DirichletBC(space, exact)], solver=("suitesparse", "umfpack"))
+print("bulk:", time.time() - start_time)
 uh = space.interpolate(0, name="solution")
 scheme.solve(target=uh)
 
 def L2(u1, u2):
     return sqrt(integrate(u1.grid, (u1-u2)**2, order=5))
 
-L2(uh, exact)
+assert( L2(uh, exact) < 1e-6 )
 
 
-# ## Solve a problem on the interface
-#
-# We can solve similar problem on the interface $\Gamma$.
 
-# In[3]:
-
-
+# Interface problem
 ispace = lagrange(igridView, order=3)
 iuh = ispace.interpolate(0, name="isolution")
 
@@ -84,31 +54,15 @@ iF = -div(grad(iexact))
 ia = inner(grad(iu), grad(iv)) * dx
 ib = iF * iv * dx
 
+start_time = time.time()
 ischeme = galerkin([ia == ib, DirichletBC(ispace, iexact)])
+print("interface:", time.time() - start_time)
 ischeme.solve(target=iuh)
-L2(iuh, iexact)
+assert( L2(iuh, iexact) < 1e-6 )
 
 
-# We can use the `plotPointData` function to visualize the solution of both grids.
 
-# In[4]:
-
-
-import matplotlib.pyplot as plt
-from dune.fem.plotting import plotPointData as plot
-
-figure = plt.figure(figsize=(3,3))
-plot(uh, figure=figure, gridLines=None)
-plot(iuh, figure=figure, linewidth=0.04, colorbar=None)
-
-
-# ## Couple bulk to surface
-#
-# We are able to compute traces of discrete functions on $\Omega$ along $\Gamma$.
-
-# In[5]:
-
-
+# Couple bulk to surface
 from dune.mmesh import trace
 tr = avg(trace(uh))
 ib = inner(grad(tr), grad(iv)) * dx
@@ -116,18 +70,12 @@ ib = inner(grad(tr), grad(iv)) * dx
 iuh.interpolate(0)
 ischeme = galerkin([ia == ib, DirichletBC(ispace, avg(trace(uh)))])
 ischeme.solve(target=iuh)
-L2(iuh, iexact)
+assert( L2(iuh, iexact) < 1e-6 )
 
 
-# ## Couple surface to bulk
-#
-# Similarly, we can evaluate a discrete function on $\Gamma$ at the skeleton of the triangulation of $\Omega$.
 
-# In[6]:
-
-
+# Couple surface to bulk
 from dune.mmesh import skeleton
-
 sk = skeleton(iuh)
 b = avg(sk) * avg(v) * dS
 
@@ -135,17 +83,55 @@ uh.interpolate(0)
 scheme = galerkin([a == b, DirichletBC(space, 0)])
 scheme.solve(target=uh)
 
-figure = plt.figure(figsize=(3,3))
-plot(uh, figure=figure, gridLines=None)
 
 
-# ## Compute jump of gradient of traces
-#
-# We can use trace and skeleton within UFL expressions. This is useful when using jump and average operators, but also to compute gradients and more.
+# Coupled solve
+from dune.mmesh import monolithicSolve, interfaceIndicator
+from dune.fem.space import dglagrange
+from dune.ufl import Constant
 
-# In[7]:
+space = dglagrange(gridView, order=3)
+u = TrialFunction(space)
+v = TestFunction(space)
+
+ispace = lagrange(igridView, order=3)
+iu = TrialFunction(ispace)
+iv = TestFunction(ispace)
+
+uh = space.interpolate(0, name="solution")
+iuh = ispace.interpolate(0, name="isolution")
+
+I = interfaceIndicator(igridView)
+n = FacetNormal(space)
+n_g = FacetNormal(ispace)
+beta = Constant(1e2, name="beta")
+omega = Constant(1e-6, name="omega")
+
+a  = inner(grad(u), grad(v)) * dx
+a += beta * inner(jump(u), jump(v)) * (1-I)*dS
+a -= dot(dot(avg(grad(u)), n('+')), jump(v)) * (1-I)*dS
+
+a += beta * inner(u - 0, v) * ds
+a -= dot(dot(grad(u), n), v) * ds
+
+a += -(skeleton(iuh) - u)('+') / omega * v('+') * I*dS
+a += -(skeleton(iuh) - u)('-') / omega * v('-') * I*dS
 
 
-from dune.mmesh import normals
-n = normals(igridView)
-jmp = jump(grad(trace(uh)), n)
+ia  = inner(grad(iu), grad(iv)) * dx
+ia += beta * inner(jump(iu), jump(iv)) * dS
+ia -= inner(avg(grad(iu)), n_g('+')) * jump(iv) * dS
+
+tr  = trace(uh)
+ia  = (iu - tr('+')) / omega * iv * dx
+ia += (iu - tr('-')) / omega * iv * dx
+
+ib  = 1 * iv * dx
+
+scheme = galerkin([a == 0])
+ischeme = galerkin([ia == ib])
+
+start_time = time.time()
+monolithicSolve(schemes=(scheme, ischeme), targets=(uh, iuh), verbose=True)
+print("monolithicSolve:", time.time() - start_time)
+assert( (max(iuh.as_numpy) - 0.14063) < 1e-3 )
